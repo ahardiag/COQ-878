14d653b27a42a30c4dd253eae977a12afa64603b
Arthur Hardiagon <ahardiag@Gmail.com>
2025-10-29 17:14:11 -0300

    Revert "change VASP paths to be used in ATOMS g3-1gpu machine"

--- set_env_var.sh
+++
@@ -1,8 +0,0 @@
-# Usage : source set_env_var.sh
-
-source /home/public755/Modules/nvidia/nvhpc/24.5/nvhpcrc
-source /home/public755/Modules/hdf5/nvc_24.5/hdf5rc
-source /home/public755/Modules/intel/oneapi/2023/mklrc
-export PATH=/home/public/Programs/vasp.6.5.1/bin/:$PATH
-
-ldd /home/public/Programs/vasp.6.5.1/bin/vasp_std | grep 'not found'
