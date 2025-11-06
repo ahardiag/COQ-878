ca79cdfe8d1210b38d089c216d7bc03a82f39ceb
Arthur Hardiagon <ahardiag@gmail.com>
2025-11-06 10:30:15 -0300

    Use spin in DFT calculations

--- set_env_var.sh
+++ set_env_var.sh
@@ -1,8 +1,8 @@
 # Usage : source set_env_var.sh
 
-source /home/public755/Modules/nvidia/nvhpc/24.5/nvhpcrc
-source /home/public755/Modules/hdf5/nvc_24.5/hdf5rc
+source /home/public755/Modules/nvidia/nvhpc/23.11/nvhpcrc
+source /home/public755/Modules/hdf5/nvc_23.11/hdf5rc
 source /home/public755/Modules/intel/oneapi/2023/mklrc
-export PATH=/home/public/Programs/vasp.6.5.1/bin/:$PATH
+export PATH=/home/public755/Programs/vasp.6.5.1/bin/:$PATH
 
-ldd /home/public/Programs/vasp.6.5.1/bin/vasp_std | grep 'not found'
+ldd /home/public755/Programs/vasp.6.5.1/bin/vasp_std | grep 'not found'
